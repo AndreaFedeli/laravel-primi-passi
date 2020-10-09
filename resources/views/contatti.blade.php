8076ee470c36a82b7a9ce1baeeba95cf0c0327cf
AndreaFedeli <andrea.fedeli159@gmail.com>
2020-10-09 14:32:13 +0200

    update

---
+++ resources/views/contatti.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title></title>
+  </head>
+  @include('.header')
+
+    <div class="rec">
+      <ul>
+        <li>C.DA COLLE DELLE API 100/A</li>
+        <li>CAMPOBASSO</li>
+        <li>Numero verde: 800.81.80.81
+            Whatsapp: 380.12.92.455</li>
+      </ul>
+    </div>
+  
+</html>
